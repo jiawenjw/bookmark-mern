2e87c3d4f77f8b54e4e0de9aa2588a70dd1adffe
Jiawen Chen <forward102910@gmail.com>
2023-06-04 13:38:43 +0100

    add google analytics

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,29 +1,33 @@
-import { useState, useEffect } from "react"
-import { BrowserRouter as Router, Route} from "react-router-dom"
-import { AuthProvider } from "./context/AuthContext"
+import { useEffect } from "react";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import { AuthProvider } from "./context/AuthContext";
+import ReactGA from "react-ga";
 
-import Homepage from "./route/Homepage"
-import Loginpage from "./route/Loginpage"
-import Registerpage from "./route/Registerpage"
-import PrivateRoute from "./utils/PrivateRoute"
+import { useLocation } from "react-router-dom";
 
+import Homepage from "./route/Homepage";
+import Loginpage from "./route/Loginpage";
+import Registerpage from "./route/Registerpage";
+import PrivateRoute from "./utils/PrivateRoute";
 
+ReactGA.initialize("G-SBC7RZWHWW");
 
 const App = () => {
+  const location = useLocation();
 
- 
-    return (
-      <Router>
-        <AuthProvider>
-          <PrivateRoute component={Homepage} path="/" exact/>
-          <Route component={Loginpage} path="/login" exact/>
-          <Route component={Registerpage} path="/register" exact/>
-        </AuthProvider>
-      </Router>
-  )
-}
+  useEffect(() => {
+    ReactGA.pageview(location.pathname + location.search);
+  }, [location]);
 
+  return (
+    <Router>
+      <AuthProvider>
+        <PrivateRoute component={Homepage} path="/" exact />
+        <Route component={Loginpage} path="/login" exact />
+        <Route component={Registerpage} path="/register" exact />
+      </AuthProvider>
+    </Router>
+  );
+};
 
-
-export default App
-
+export default App;
