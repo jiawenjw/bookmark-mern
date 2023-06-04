ad0b44bed6cac4071b09d9314e1de3199ac2e365
Jiawen Chen <forward102910@gmail.com>
2023-06-04 14:54:22 +0100

    add google analytics

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,24 +1,12 @@
-import { useEffect } from "react";
 import { BrowserRouter as Router, Route } from "react-router-dom";
 import { AuthProvider } from "./context/AuthContext";
-import ReactGA from "react-ga";
-
-import { useLocation } from "react-router-dom";
 
 import Homepage from "./route/Homepage";
 import Loginpage from "./route/Loginpage";
 import Registerpage from "./route/Registerpage";
 import PrivateRoute from "./utils/PrivateRoute";
 
-ReactGA.initialize("G-SBC7RZWHWW");
-
 const App = () => {
-  const location = useLocation();
-
-  useEffect(() => {
-    ReactGA.pageview(location.pathname + location.search);
-  }, [location]);
-
   return (
     <Router>
       <AuthProvider>
